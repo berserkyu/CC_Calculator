0bb5ba34058f1b5957cce592db5cd4666cb99557
KohZhen <79892482+KohZhen@users.noreply.github.com>
2021-03-04 22:40:12 +0800

    Add files via upload

---
+++ widget.h
@@ -0,0 +1,71 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QStack>
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = 0);
+    ~Widget();
+    void getChar();//将QString转换为QByteArray
+    void getSuffix();//获取后缀表达式
+    void m_init();//初始化
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_5_clicked();
+
+    void on_pushButton_6_clicked();
+
+    void on_pushButton_7_clicked();
+
+    void on_pushButton_8_clicked();
+
+    void on_pushButton_9_clicked();
+
+    void on_pushButton_10_clicked();
+
+    void on_pushButton_11_clicked();
+
+    void on_pushButton_12_clicked();
+
+    void on_pushButton_13_clicked();
+
+    void on_pushButton_14_clicked();
+
+    void on_pushButton_15_clicked();
+
+    void on_pushButton_20_clicked();
+
+    void on_pushButton_18_clicked();
+
+    void on_pushButton_19_clicked();
+
+    void on_pushButton_16_clicked();
+
+    void on_pushButton_17_clicked();
+
+private:
+    Ui::Widget *ui;
+    QString in;//输入的表达式
+    QByteArray ba;//输入表达式转换为字节数组
+    QStack<char> s1;//中缀表达式转换为后缀表达式时用到的叠层
+    QVector<QString> out;//后缀表达式
+
+};
+
+#endif // WIDGET_H
